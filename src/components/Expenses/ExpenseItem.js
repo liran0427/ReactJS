489b6193a3a05a42e8f88b0cce9b2871d1df70b6
liran0427 <liran0427@gmail.com>
2022-12-25 13:17:45 -0700

    listen to events & working with event handler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,6 +2,10 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
+  const clickHandler =() => {
+    console.log('Clicked!!!');
+  };
+
   return (
     <Card className="expense-item">
         <ExpenseDate date={props.date}></ExpenseDate>
@@ -9,6 +13,7 @@ function ExpenseItem(props) {
         <h2> {props.title} </h2>
         <div className="expense-item__price"> ${props.amount} </div>
       </div>
+      <button onClick={clickHandler()}>Change Title</button>
     </Card>
   );
 }
