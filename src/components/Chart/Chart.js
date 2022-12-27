919d959bec788ddcb66dc90f02e8d9453869eff2
liran0427 <liran0427@gmail.com>
2022-12-26 22:20:25 -0700

    add dynamic style to chartbar

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,6 +3,12 @@ import ChartBar from ".ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  let barFillHight = "0%";
+
+  if (props.max > 0) {
+    barFillHight = Math.round((props.value / props.maxValue) * 100) + "%";
+  }
+
   return <div className="chart"></div>;
   {
     props.dataPoints.map((dataPoint) => (
